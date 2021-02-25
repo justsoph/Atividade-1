9988b95258f1b0e2156522107c1e2a48789e3a15
justsoph <sophiaanaklumythic@gmail.com>
2021-02-26 07:26:27 +0900

    Primeira parte do projeto de calculadora.

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,9 @@
 // This is a JavaScript file
 
+$(document).on("click", "#somar", function(){
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+    
+    var resultSoma = parseFloat(valor1) + parseFloat(valor2);
+    $(resultSoma).val(resultSoma);
+});
