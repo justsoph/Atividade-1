62beb5ce396f1fff5e1d71400884df776c4ac5e1
justsoph <sophiaanaklumythic@gmail.com>
2021-03-05 05:11:29 +0900

    Atividade 1 de App Mobile

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,9 +1,34 @@
 // This is a JavaScript file
 
-$(document).on("click", "#somar", function(){
-    var valor1 = $("#valor1").val();
-    var valor2 = $("#valor2").val();
-    
-    var resultSoma = parseFloat(valor1) + parseFloat(valor2);
-    $(resultSoma).val(resultSoma);
+$(document).on('click', '#somar', function(){
+    var valor1 = $('#valor1').val();
+    var valor2 = $('#valor2').val();
+
+    var resultado = parseFloat(valor1) + parseFloat(valor2)
+
+    $('#resultado').val(resultado);
+});
+$(document).on('click', '#subtrair', function(){
+    var valor1 = $('#valor1').val();
+    var valor2 = $('#valor2').val();
+
+    var resultado = parseFloat(valor1) - parseFloat(valor2)
+
+    $('#resultado').val(resultado);
+});
+$(document).on('click', '#multiplicar', function(){
+    var valor1 = $('#valor1').val();
+    var valor2 = $('#valor2').val();
+
+    var resultado = parseFloat(valor1) * parseFloat(valor2)
+
+    $('#resultado').val(resultado);
+});
+$(document).on('click', '#dividir', function(){
+    var valor1 = $('#valor1').val();
+    var valor2 = $('#valor2').val();
+
+    var resultado = parseFloat(valor1) / parseFloat(valor2)
+
+    $('#resultado').val(resultado);
 });
